1a5638ee8244db9f9dcfdb41195aa7c61dcedbf9
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-12-14 11:20:39 -0800

    adds Profile to admin

--- bytes/models.py
+++ bytes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Profile model for storing the user's contact info
+class Profile(models.Model):
+    # Users do not get deleted - only set to inactive
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    add1 = models.CharField(max_length = 200)
+    add2 = models.CharField(max_length = 200)
+    city = models.CharField(max_length = 200)
+    # Should state use a tuple to set choices?
+    state = models.CharField(max_length = 2)
+    zip = models.IntegerField(min = 0, max = 99999)
+
+    def __str__(self):
+        return f'{self.add1}, {self.add2}, {self.city}, {self.state} {self.zip}'
